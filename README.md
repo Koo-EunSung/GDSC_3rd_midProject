e2cce2f9a4451b37ee712edc40fbef56d01a496a
Koo-EunSung <ku903@naver.com>
2024-01-02 22:31:07 +0900

    project intiailize

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GDSC_3rd_midProject
+
+GDSC 3기 BE 4팀에서 스터디 중 진행한 중간 프로젝트입니다.
---
+++ src/test/java/gdsc/midproject/MidprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.midproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
